74291109c50627f89482ae4b98b3e6b7c9d38c50
sangeethamoorthy2023 <144022982+sangeethamoorthy2023@users.noreply.github.com>
2023-09-29 16:23:48 +0530

    Update transalate.py
    save

--- transalate.py
+++ transalate.py
@@ -11,4 +11,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=8080)
+# you can add inboound rule in networking
